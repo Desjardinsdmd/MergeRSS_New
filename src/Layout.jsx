33f7dbbafc8dbbaed73a6802006dae4e4460c34e
Dylan Desjardins <desjardinsdmd@gmail.com>
2026-02-27 16:17:43 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -24,6 +24,7 @@ import {
 import { Button } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
 import { useTheme } from 'next-themes';
+import { ThemeProvider } from './ThemeProvider';
 
 const navigation = [
   { name: 'Dashboard', href: 'Dashboard', icon: LayoutDashboard },
