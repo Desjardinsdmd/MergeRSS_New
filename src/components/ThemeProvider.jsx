2ca9c9496c18df75ddebe8828f36f6b0ac9e517a
Dylan Desjardins <desjardinsdmd@gmail.com>
2026-02-27 16:17:13 +0000

    File changes

---
+++ src/components/ThemeProvider.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from 'react';
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
+
+export function ThemeProvider({ children }) {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return children;
+
+  return (
+    <NextThemesProvider attribute="class" defaultTheme="light" enableSystem storageKey="theme">
+      {children}
+    </NextThemesProvider>
+  );
+}
